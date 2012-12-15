e026b1c2960dbd2c60a3f4ebdcaa18055e40ecf3
Domokos Molnar <domokos@gmail.com>
2012-12-15 09:12:53 +0100

    Add utils

--- src/G729split.c
+++ src/G729split.c
@@ -10,6 +10,16 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+#include <math.h>
+#include <time.h>
+
+
+#include "utils.h"
+
+#include "bcg729/decoder.h"
+
 
 int main(void) {
 	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
